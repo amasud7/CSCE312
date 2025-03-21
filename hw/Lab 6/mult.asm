9cd6430c468b2d6a1c59e0f35d8acd75e4b54b2b
Ayad Masud <ayad.r.masud@gmail.com>
2025-03-20 22:00:34 -0500

    finished lab 6

--- hw/Lab 6/mult.asm
+++ hw/Lab 6/mult.asm
@@ -1,10 +1,44 @@
 //mult.asm
 
-//NAME: 
-//UIN: 
+//NAME: Ayad Masud
+//UIN: 733009045
 
 //This asm computes the mult of two numbers
 //Assuming R0 stores the value n1, R1 stores value n2 
 //and the final result is stored in R2.
 ///////////////////////////////////////////////////////////////////////////////
 
+// initalize the sum to 0
+@R2
+M=0
+
+// load R1 (multiplier) // we need to load r1 into a different register and then decrement that
+@R1
+D=M
+@C
+M=D
+
+// check if R1 is zero --> then just return 0 because 0 * n = 0
+@end
+D;JEQ
+
+(loop)
+    // load R0 (multiplicand)
+    @R0
+    D=M
+
+    @R2
+    M=M+D // add R0 to R2
+
+    // decrement R1
+    @C
+    M=M-1
+    D=M // put in d register to check if R1 is 0
+
+    // run loop until R1 is 0
+    @loop
+    D;JGT
+
+(end)
+    @end // infinite loop
+    0;JMP
--- hw/Lab 6/mult.tst
+++ hw/Lab 6/mult.tst
@@ -1,6 +1,6 @@
 load mult.asm,
 output-file mult.out,
-//compare-to mult.cmp,
+compare-to mult.cmp,
 output-list RAM[0]%D2.6.2 RAM[1]%D2.6.2 RAM[2]%D2.6.2;
 
 set PC 0,
